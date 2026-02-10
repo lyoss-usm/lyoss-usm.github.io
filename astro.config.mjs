0415a78775a682baaf64d277efc1726a3f61fefc
Mitchel Rojas <mrojasr@usm.cl>
2026-02-10 01:51:47 -0300

    chore: add sitemap library

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,16 +5,18 @@ import tailwindcss from '@tailwindcss/vite';
 
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		plugins: [tailwindcss()]
-	},
-	image: {
-		domains: ['avatars.githubusercontent.com', 'github.com']
-	},
-	output: 'static',
-	site: 'https://lyoss-usm.github.io',
-	compressHTML: true,
-	integrations: [mdx()]
+    vite: {
+        plugins: [tailwindcss()]
+    },
+    image: {
+        domains: ['avatars.githubusercontent.com', 'github.com']
+    },
+    output: 'static',
+    site: 'https://lyoss.org',
+    compressHTML: true,
+    integrations: [mdx(), sitemap()]
 });
